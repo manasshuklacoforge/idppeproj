95d787156f1649c7e2c2bbec5a35ee159ac96e55
manasshuklacoforge <manas.shukla@coforge.com>
2025-05-22 17:30:43 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,27 +1,32 @@
 terraform {
- required_providers {
-   azurerm = {
-     source = "hashicorp/azurerm"
-     version = "3.78.0"
-   }
- }
-}
-provider "azurerm" {
-  features {}
-  subscription_id = "a327a41f-a5e9-458f-b5bb-0dc87483eb85"
-}
-# Create a resource group
-resource "idppeproj_resource_group" "example" {
-  name     = "idppeproj-resource-group"
-  location = "eastus" # Choose your desired location
-}
-# Create an Azure Storage Account
-resource "azurerm_storage_account" "example" {
-  name                     = "idppeprojstorageaccount" # Storage account name (must be globally unique)
-  resource_group_name      = idppeproj_resource_group.example.name
-  location                 = idppeproj_resource_group.example.location
-  account_kind             = "StorageV2" # Or "BlobStorage", "FileStorage", etc.
-  account_replication_type = "LRS"  # Or "GRS", "ZRS", etc.
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
+}
+
+# Configure the Azure Provider
+provider "azurerm" {
+  feature {
+    storage_account_enable_public_access_by_default = false
+  }
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "EastUS"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                = "mystorageaccount123"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  account_kind        = "StandardLRS"
+  # ... other settings
+}
   
 
